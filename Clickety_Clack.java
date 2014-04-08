fb654291f8c50b300d3a6ac947615128896dd3f8
jcapra37 <thethespian37@gmail.com>
2014-04-08 17:21:33 -0400

    Update Clickety_Clack.java
    Updated the main to contain the new rules class

--- Clickety_Clack.java
+++ Clickety_Clack.java
@@ -37,28 +37,8 @@ public static void main(String[] main)
 	}
 	else{System.out.println("Error! Please enter a value between 2 and 4.");}
 			}
-	int ruleLoop=1;
-	while(ruleLoop==1)
-		{
-	Scanner keys=new Scanner(System.in);
-	String famRules;
-	System.out.println("Are you familiar with the rules? [Y/N]");
-	famRules=keys.nextLine();
-	if(famRules.equalsIgnoreCase("y")) //playgame
-	{
-		System.out.println("Great! Then let's get started.");
-		ruleLoop++;
-	}
-	else if(famRules.equalsIgnoreCase("n")) //explain rules
-	{
-		System.out.print("Not a problem, the rules are simple!\nThere are five dice in this game.\nThe object of the game is to roll a six first, then a five, and then a four.");
-		System.out.println("\nAfterwards you aim to roll the highest number with the two remaining dice.\nThis number represents your score, which is compared to the other player(s).");
-	}
-	else //error
-	{
-		System.out.println("Error! please enter a correct response! [Y/N]");
-	}
-		}
+	Rules theRules=new Rules();
+	theRules.daRules();
 	final int COL=players;
 	final int ROW=5;
 	int num;
