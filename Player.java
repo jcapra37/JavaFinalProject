db002fa1688156e9505a3423d4367ae931c7909b
ultamashot <ULTAMASHOT@GMAIL.COM>
2014-04-07 21:03:53 -0400

    This is a test commit
    Test commit of player class

---
+++ Player.java
@@ -0,0 +1,62 @@
+package Java_Final;
+
+import java.util.Scanner;
+
+public class Player {
+	Boolean look_6=false;
+	Boolean look_5=false;
+	Boolean look_4=false;
+	int[] playerdie=new int[4];
+	private int playernumber;
+	int score;
+	int rolls;
+	void setName( int order_number )
+	{
+		playernumber = order_number;
+	}
+	int getscore()
+	{
+		System.out.println( "You have "+score+" points");
+	}
+	
+	if(look_6=true)
+	{
+		playerdie[0]=6;
+	
+		if(look_5=true)
+		{
+			playerdie[1]=5;
+			if(look_4=true)
+			{
+				playerdie[2]=4;
+				score = playerdie[3]+playerdie[4];
+				System.out.println( "You have "+score+" points");
+				if(rolls<3)
+				{
+					System.out.println("You have "+rolls+" left");
+					System.out.println("Would you like to reroll?");
+					System.out.println("y/n");
+					Scanner keys=new Scanner(System.in);
+					String reroll;
+					reroll=keys.nextLine();
+					if(reroll.equalsIgnoreCase("y")) //playgame
+					{
+//						reroll playerdie[3]+playerdie[4];
+					}
+					else if(reroll.equalsIgnoreCase("n")) //explain rules
+					{
+						System.out.print("Your score for this round is set.");
+					}
+					else //error
+					{
+						System.out.println("Error! please enter a correct response! [Y/N]");
+					}
+				}
+					
+			}
+			
+		}
+	}
+	}
+
+}
