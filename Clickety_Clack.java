9a81668a1ac831734fb0ef881c1d9afaedd11971
jcapra37 <thethespian37@gmail.com>
2014-04-21 17:03:03 -0400

    Update Clickety_Clack.java
    Updated to be cleaner and behave a little better.

--- Clickety_Clack.java
+++ Clickety_Clack.java
@@ -22,41 +22,39 @@ System.out.println("How many players do you have?\nAt least 2 players are needed
 players=keyboard.nextInt();
 if(players==2)
 {
-	players=2;
+players=2;
 int player1; int player2;
-	play_loop=2;
+play_loop=2;
 }
 else if(players==3)
 {
-	players=3;
+players=3;
 int player1; int player2; int player3;
 play_loop=2;
 }
 else if(players==4)
 {
-	players=4;
+players=4;
 int player1; int player2; int player3; int player4;
 play_loop=2;
 }
 else{System.out.println("Error! Please enter a value between 2 and 4.");}
 }
-Rules theRules=new Rules();
-theRules.daRules();
-final int COL=players;
-final int ROW=5;
-int num;
-int[][] dice_Storage = new int[ROW][COL];
-//Player plays = new Player();
-Dice_Clickety rollyPolly = new Dice_Clickety();
-//plays.setName(player1);
-rollyPolly.roll1();
-System.out.println(rollyPolly.Dice_Roll());
+	Rules theRules=new Rules();
+	theRules.daRules();
+	final int COL=players;
+	final int ROW=5;
+	int num;
+	int[][] dice_Storage=new int[ROW][COL];
+	Dice_Clickety rollyPolly= new Dice_Clickety();
+	rollyPolly.roll1();
+	System.out.println(rollyPolly.Dice_Roll());
 
 }
+
 catch(Exception e){System.out.println("An unexpected error has occured. Please restart the program.");}
 
 }
 }
-
 
 
