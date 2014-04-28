f07e50591e4c9d777db3f8f43de8fc7bc89bf3a1
jcapra37 <thethespian37@gmail.com>
2014-04-28 16:21:15 -0400

    Update Clickety_Clack.java
    Fixed a stupid mistake where instead of declaring multiple instances of the player class in the main I assigned their values to ints leading to a massive crap storm of no return.

--- Clickety_Clack.java
+++ Clickety_Clack.java
@@ -13,7 +13,6 @@ public static void main(String[] main)
 try
 {
 Scanner keyboard=new Scanner(System.in);
-int players=1;
 int play_loop=1;
 System.out.println("Greetings! Welcome to our game of Clickety Clack.");
 while(play_loop==1)
@@ -22,39 +21,38 @@ System.out.println("How many players do you have?\nAt least 2 players are needed
 players=keyboard.nextInt();
 if(players==2)
 {
-players=2;
-int player1; int player2;
+Player player1=new Player();
+Player player2=new Player();
 play_loop=2;
 }
 else if(players==3)
 {
-players=3;
-int player1; int player2; int player3;
+Player player1=new Player();
+Player player2=new Player();
+Player player3=new Player();
 play_loop=2;
 }
 else if(players==4)
 {
-players=4;
-int player1; int player2; int player3; int player4;
+Player player1=new Player();
+Player player2=new Player();
+Player player3=new Player();
+Player player4=new Player();
 play_loop=2;
 }
 else{System.out.println("Error! Please enter a value between 2 and 4.");}
 }
-	Rules theRules=new Rules();
-	theRules.daRules();
-	final int COL=players;
-	final int ROW=5;
-	int num;
-	int[][] dice_Storage=new int[ROW][COL];
-	Dice_Clickety rollyPolly= new Dice_Clickety();
-	rollyPolly.roll1();
-	System.out.println(rollyPolly.Dice_Roll());
-
+Rules theRules=new Rules();
+theRules.daRules();
+final int COL=players;
+final int ROW=5;
+int num;
+int[][] dice_Storage=new int[ROW][COL];
+Dice_Clickety rollyPolly= new Dice_Clickety();
+rollyPolly.roll1();
+System.out.println(rollyPolly.Dice_Roll());
 }
-
 catch(Exception e){System.out.println("An unexpected error has occured. Please restart the program.");}
 
 }
 }
-
-
